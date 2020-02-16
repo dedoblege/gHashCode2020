5fbab5047dade7bb6084f5fbd0af126ef44675e4
Delia <dedoblege@gmail.com>
2020-02-16 19:14:59 +0100

    combinations solution

--- pizzaProblemSolver/src/com/example/sliceCalculator.java
+++ pizzaProblemSolver/src/com/example/sliceCalculator.java
@@ -1,111 +1,145 @@
 package com.example;
 
-import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.List;
 
 public class sliceCalculator {
-    public static void main(String [] args) {
+    public static void main(String [] args) throws IOException {
+
 
         //TODO Read file a_example
 
+       
+
         //TODO convert string to int
 
-        int maxSlices = 17;
-        int numberOfTypes = 4;
-        int[] inSlicesType = {2, 5, 6, 8};
+        int maxSlices = 100;
+        int numberOfTypes = 10;
+        int[] inSlices = {4, 14, 15, 18, 29, 32, 36, 82, 95, 95};
 
-        String outLine1 = "";
-        String outLine2 = "";
+//        int maxSlices = 17;
+//        int numberOfTypes = 4;
+//        int [] inSlices = {2,5,6,8};
+//        ArrayList<Integer> inSlices = new ArrayList<Integer>(numberOfTypes);
+//        inSlices.add(2);
+//        inSlices.add(5);
+//        inSlices.add(6);
+//        inSlices.add(8);
 
         int resultNumberOfTypes = 0;
-        ArrayList<Integer> outVectorTypes = new ArrayList<Integer>();
+
+        int actualMaxForThisR;
+        int actualMax;
+        ArrayList<Integer> actualCombiForThisR = new ArrayList<>();
+        ArrayList<Integer> actualCombi = new ArrayList<>();
+
+        ArrayList<ArrayList<Integer>> combinationsForR = new ArrayList<>();
 
         int maxSlicesToGet = maxSlices;
         int totalNumberOfSlices = 0;
 
-        //slice calculator
-        for(int positionInSlicesType=numberOfTypes-1; positionInSlicesType>=0; positionInSlicesType--) {
-            //number of pizzas of the bigger pizza
-            int numberOfPizza = maxSlicesToGet / inSlicesType[positionInSlicesType];
-            //number of slices not covered yet
-            int slicesToCover = maxSlicesToGet % inSlicesType[positionInSlicesType];
+        actualMax = 0;
+        for(int r=0;r<numberOfTypes;r++) {
+            actualMaxForThisR = 0;
+            actualCombiForThisR.clear();
 
-            if(numberOfPizza > 0) {
-                if (slicesToCover < inSlicesType[0] && slicesToCover > 0) {
-                    numberOfPizza--;
-                } else if (slicesToCover == 0) {
-                    //maxSlices reached --> we stop here
-                    break;
+            combinationsForR = printCombination(inSlices, numberOfTypes, r);
+
+            for (ArrayList<Integer> eachCombi : combinationsForR) {
+                if (sum(eachCombi) > actualMaxForThisR
+                        && sum(eachCombi) <= maxSlices) {
+                    actualMaxForThisR = sum(eachCombi);
+                    actualCombiForThisR.clear();
+                    actualCombiForThisR.addAll(eachCombi);
                 }
-
-                //save result
-                resultNumberOfTypes++;
-                outVectorTypes.add(positionInSlicesType);
-
-                //set max to next iteration
-                maxSlicesToGet = maxSlicesToGet - (numberOfPizza * inSlicesType[positionInSlicesType]);
-
-                //save for verifying
-                totalNumberOfSlices += numberOfPizza * inSlicesType[positionInSlicesType];
             }
+            System.out.println("best combi for this r(" + r +") : " + actualCombiForThisR);
+
+            if(r==0){
+                actualMax = actualMaxForThisR;
+                actualCombi.addAll(actualCombiForThisR);
+            }
+
+            if(sum(actualCombiForThisR) > actualMax
+                    && sum(actualCombiForThisR) <= maxSlices){
+                actualMax = sum(actualCombiForThisR);
+                actualCombi.clear();
+                actualCombi.addAll(actualCombiForThisR);
+            }
+
         }
 
-        //verifying
-        if(totalNumberOfSlices <= maxSlices){
-            //order vector ascending before showing solution
-            Collections.sort(outVectorTypes);
-
-
-            System.out.println("resultNumberOfTypes = " + resultNumberOfTypes);
-            System.out.println("typesVector = " + outVectorTypes);
+        System.out.println("final decision : " + actualCombi);
 
             //TODO write result in file
-        }else{
-            System.out.println("ERROR -  MAX exceeded! Your result is not correct!");
-            System.out.println("You should repeat the calculation");
+
+    }
+
+    /*  https://www.geeksforgeeks.org/print-all-possible-combinations-of-r-elements-in-a-given-array-of-size-n/
+    */
+    /** arr[]  ---> Input Array
+        data[] ---> Temporary array to store current combination
+        start & end ---> Staring and Ending indexes in arr[]
+        index  ---> Current index in data[]
+        r ---> Size of a combination to be printed */
+    static void combinationUtil(int arr[], int n, int r, int index,
+                                int data[], int i, ArrayList<ArrayList<Integer>> vectorOfVectors)
+    {
+        ArrayList<Integer> myVector = new ArrayList<>();
+
+        // Current combination is ready to be printed, print it
+        if (index == r)
+        {
+            for (int j=0; j<r; j++)
+                myVector.add(data[j]);
+
+            vectorOfVectors.add(myVector);
+
+            return;
         }
 
+        // When no more elements are there to put in data[]
+        if (i >= n)
+            return;
 
-//        // The name of the file to open.
-//        String fileName = "a_example.txt";
-//
-//        // This will reference one line at a time
-//        String line = null;
-//
-//        try {
-//            // Use this for reading the data.
-//            byte[] buffer = new byte[1000];
-//
-//            FileInputStream inputStream =
-//                    new FileInputStream(fileName);
-//
-//            // read fills buffer with data and returns
-//            // the number of bytes read (which of course
-//            // may be less than the buffer size, but
-//            // it will never be more).
-//            int total = 0;
-//            int nRead = 0;
-//            while((nRead = inputStream.read(buffer)) != -1) {
-//                // Convert to String so we can display it.
-//                // Of course you wouldn't want to do this with
-//                // a 'real' binary file.
-//                System.out.println(new String(buffer));
-//                total += nRead;
-//            }
-//
-//            // Always close files.
-//            inputStream.close();
-//
-//            System.out.println("Read " + total + " bytes");
-//        } catch (FileNotFoundException ex) {
-//            System.out.println(
-//                    "Unable to open file '" +
-//                            fileName + "'");
-//        } catch (IOException ex) {
-//            System.out.println(
-//                    "Error reading file '"
-//                            + fileName + "'");
-//        }
+        // current is included, put next at next location
+        data[index] = arr[i];
+        combinationUtil(arr, n, r, index+1, data, i+1, vectorOfVectors);
+
+        // current is excluded, replace it with next (Note that
+        // i+1 is passed, but index is not changed)
+        combinationUtil(arr, n, r, index, data, i+1,vectorOfVectors);
     }
+
+    /** The main function that prints all combinations of size r
+     in arr[] of size n. This function mainly uses combinationUtil() */
+    static  ArrayList<ArrayList<Integer>> printCombination(int arr[], int n, int r)
+    {
+        ArrayList<ArrayList<Integer>> combinationsForR = new ArrayList<>();
+
+        // A temporary array to store all combination one by one
+        int data[]=new int[r];
+
+        // Print all combination using temporary array 'data[]'
+        combinationUtil(arr, n, r, 0, data, 0, combinationsForR);
+        System.out.println(combinationsForR); //
+
+        return combinationsForR;
+    }
+
+    static int sum(List<Integer> list) {
+        int sum = 0;
+
+        if(!list.isEmpty()) {
+            for (int i : list)
+                sum = sum + i;
+        }
+        return sum;
+    }
+
+
+
 }
